a6f619814fbc8ec1c344908bae2d3e16254041cc
anishkha02 <anishkha0208@gmail.com>
2024-01-09 20:17:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerceapp/constants/global_variables.dart';
+import 'package:ecommerceapp/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerceapp/router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme:const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black,
+          )
+        )
+      ),
+      onGenerateRoute: (settings)=> generateRoute(settings),
+      home: AuthScreen(),
+    );
+  }
+}
