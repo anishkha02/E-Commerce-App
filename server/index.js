ea7d262ddd5bd93207a9028aa5d4ae1b07f02a3f
anishkha02 <anishkha0208@gmail.com>
2024-02-13 18:43:00 +0530

    server code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
-const express=require('express');
-const PORT=3000;
-const app=express();
+//import from packages just like dart bt in js
+const express = require('express');
+const mongoose = require("mongoose");
 
-app.listen(PORT, ()=>{
-   console.log(`connected at port ${PORT}`);
+//imports from other files
+const authRouter = require("./routes/auth");
+
+//init
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://anishkha0208:anishkha123@cluster0.hfqdhfn.mongodb.net/ecommerce";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+//connections
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
 });
